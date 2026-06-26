4d5245348561842cb91c6483736e5f4bfdd71059
Zentro-Works <zw.zentro@gmail.com>
2026-06-26 22:21:31 +0530

    fix: update confis t ouse env vars for cloud deploy

--- fitness-app-frontend/src/authConfig.js
+++ fitness-app-frontend/src/authConfig.js
@@ -1,8 +1,8 @@
 ﻿export const authConfig = {
   clientId: 'oauth2-pkce-client',
-  authorizationEndpoint: ${VITE_KEYCLOAK_URL}/realms/fitness-oauth2/protocol/openid-connect/auth,
-  tokenEndpoint: ${VITE_KEYCLOAK_URL}/realms/fitness-oauth2/protocol/openid-connect/token,
-  redirectUri: ${VITE_REDIRECT_URI},
+  authorizationEndpoint: '${VITE_KEYCLOAK_URL}/realms/fitness-oauth2/protocol/openid-connect/auth',
+  tokenEndpoint: '${VITE_KEYCLOAK_URL}/realms/fitness-oauth2/protocol/openid-connect/token',
+  redirectUri: window.location.origin,
   scope: 'openid profile email offline_access',
   onRefreshTokenExpire: (event) => event.logIn(),
 }
